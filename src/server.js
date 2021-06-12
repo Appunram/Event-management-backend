cf0be435a4d12d74af9365a9d004ea10b3b9d30f
Appunram <appunram@gmail.com>
2021-06-12 11:20:15 +0530

    update server.js

--- src/server.js
+++ src/server.js
@@ -29,11 +29,11 @@ try {
 }
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("frontend/build"));
+    app.use(express.static("client/build"));
 }
 app.use(routes);
 app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 
