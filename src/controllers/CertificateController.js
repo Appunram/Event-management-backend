9e1fa8c72943a04aaf0a8c7652c927622067e438
Appunram <appunram@gmail.com>
2021-06-18 12:27:12 +0530

    changes

--- src/controllers/CertificateController.js
+++ src/controllers/CertificateController.js
@@ -1,10 +1,25 @@
-const certificategenerator = require("../utils/CertificateGenerator");
+const moment = require("moment");
+const PDFDocument = require("pdfkit");
 
 module.exports = {
     async generatecertificate(req,res){
         try {
+            const doc = new PDFDocument({
+                layout: "landscape",
+                size: "A4",
+            });
             const {name} = req.body;
-            const certificate = await certificategenerator(name);
+            res.setHeader("Content-Type","application/pdf");
+            res.setHeader("Content-Disposition",`attachment; filename=${name}.pdf`);
+            doc.pipe(res);
+            doc.image("images/certificate.png", 0, 0, { width: 842 });
+            doc.fontSize(60).text(name, 20, 265, {
+                align: "center"
+            });
+            doc.fontSize(17).text(moment().format("MMMM Do YYYY"), -275, 430, {
+                align: "center"
+            });
+            doc.end();
             res.status(200).json({ message: "certificate generated successfully"})
         } catch (error) {
             console.log(error)
